d55aef375037d3832e0c5286c6a055edffbed211
Tero Saarimaa <tmsaarimaa@gmail.com>
2018-08-17 15:35:09 +0300

    jep

--- speedseat.js
+++ speedseat.js
@@ -1,5 +1,5 @@
 var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World!');
+    res.end('Hello World v2!');
 }).listen(8080);
